d0b65abdfb22b4a8fcbd655bf9077de6b3b3c912
Женя Балабанов <biomannumberone@gmail.com>
2024-04-06 12:49:46 +0300

    added axios and api

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,52 @@
 <template>
-  <div>
-    <h1><p class="header">Hello world!</p></h1>
+  <div class="wrapper">
+    <div class="container"></div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { onMounted, ref } from "vue";
+import axios from "axios";
 
-<style lang="scss">
-$main-color: red;
-$main-white: white;
+const lat = ref(-90);
+const lon = ref(100);
+const apiKey = ref("1GJZNmbjhPbYpQ96kUtRGujyRcRg6n2PeW");
+const tempMoscow = ref(0);
 
-body {
-  background-color: $main-color;
+async function getWeather() {
+  try {
+    const response = await axios.get(
+      "https://api.openweathermap.org/data/2.5/weather?q=Moscow&units=metric&appid=3c3c89550e3a518b73d3d3920e7cf5d6"
+    );
+
+    tempMoscow.value = response.data.main.temp;
+
+    console.log(response.data.coord);
+    console.log(`Температура москвы: ${response.data.main.temp}`);
+  } catch (error) {
+    console.log("Error");
+  }
 }
 
-.header {
-  color: $main-white;
+onMounted(() => {
+  getWeather();
+});
+</script>
+
+<style lang="scss">
+@import "./scss/variables.scss";
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.container {
+  height: 600px;
+  width: 1000px;
+  margin: 0 auto;
+  border-radius: 20px;
+  background: $--color-powder-blue;
 }
 </style>
