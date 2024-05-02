61ceb98113dd86077ba326dfab2d75dc48a38422
Женя Балабанов <biomannumberone@gmail.com>
2024-05-02 14:47:28 +0300

    fixed

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,8 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  publicPath:
+    process.env.NODE_ENV === "production" ? "/TrackWeather.github.io/" : "/",
   plugins: [vue()],
   resolve: {
     alias: {
