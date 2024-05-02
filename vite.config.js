364ca785aa6c3ae983b6832f1a2b1591a237c343
Женя Балабанов <biomannumberone@gmail.com>
2024-05-02 14:23:56 +0300

    fixed

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,23 @@
 import { fileURLToPath, URL } from "node:url";
+import externalGlobals from "rollup-plugin-external-globals";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      external: ["vue", "element-plus", "vue-demi"],
+      plugins: [
+        externalGlobals({
+          vue: "Vue",
+          "element-plus": "ElementPlus",
+          "vue-demi": "VueDemi",
+        }),
+      ],
+    },
+  },
   plugins: [vue()],
   resolve: {
     alias: {
